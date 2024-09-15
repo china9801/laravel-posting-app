811612e3e6a313be53628f0d104a10abb39967b9
china9801 <x17f007@gmail.com>
2024-09-15 09:53:44 +0900

    ルーティングの設定追加

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use App\Http\Controllers\PostController;
 |
 */
 
+
+Route::get('/', [PostController::class, 'index'])->middleware(['auth', 'verified'])->name('posts.index');
 /*
 Route::get('/dashboard', function () {
     return view('dashboard');
